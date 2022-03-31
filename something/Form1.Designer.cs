7c819a7eb9a4537a7c53ce45d9d558ea7672e269
Вячеслав Суханов <slavinapochta77@outlook.com>
2022-03-31 22:01:04 +0300

    Добавьте файлы проекта.

---
+++ something/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace something
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(16, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.treeView1.Location = new System.Drawing.Point(16, 57);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(4);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(285, 651);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(124, 17);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(711, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(311, 57);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(561, 650);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Deleted);
+            this.fileSystemWatcher1.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Deleted);
+            this.fileSystemWatcher1.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Deleted);
+            this.fileSystemWatcher1.Renamed += new System.IO.RenamedEventHandler(this.fileSystemWatcher1_Renamed);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem3});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(154, 26);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(153, 22);
+            this.toolStripMenuItem3.Text = "Удалить папку";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(210, 24);
+            this.toolStripMenuItem2.Text = "123";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(842, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 28);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 724);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ something/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace something
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string stpa;
+            StreamReader sr = new StreamReader("save.txt");
+            stpa = sr.ReadLine();
+            sr.Close();
+            FileSystemWatcher watcher = new FileSystemWatcher(stpa);
+            DirectoryInfo dir = new DirectoryInfo(stpa);
+            foreach (var item in dir.GetDirectories())
+            {
+                TreeNode tn = new TreeNode { Text = item.Name, Name = item.FullName };
+                treeView1.Nodes.Add(tn);
+            }
+            foreach (var item in dir.GetFiles())
+            {
+                listView1.Items.Add(item.Name);
+            }
+            fileSystemWatcher1.Path = dir.ToString();
+            textBox1.Text = dir.ToString();
+            treeView1.ContextMenuStrip = contextMenuStrip1;
+        }
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (button2.Enabled == false)
+            {
+
+                try
+                {
+                    DirectoryInfo dir = new DirectoryInfo(e.Node.Name);
+                    
+                    dir.GetDirectories();
+                    FileSystemWatcher watcher = new FileSystemWatcher(e.Node.Name);
+                    treeView1.Nodes.Clear();
+                    foreach (var item in dir.GetDirectories())
+                    {
+                        TreeNode tn = new TreeNode { Text = item.Name, Name = item.FullName };
+                        treeView1.Nodes.Add(tn);
+                    }
+                    listView1.Items.Clear();
+                    foreach (var item in dir.GetFiles())
+                    {
+                        listView1.Items.Add(item.Name);
+                    }
+                    fileSystemWatcher1.Path = dir.ToString();
+                    textBox1.Text = dir.ToString();
+
+                }
+                catch { };
+                button2.Enabled = true;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button2.Enabled == true)
+            {
+                string path = textBox1.Text;
+                int i = path.Length - 1;
+                while (path[i] != '\\')
+                {
+                    path = path.Remove(i);
+                    i--;
+                }
+                if (path != @"C:\") path = path.Remove(i);
+                DirectoryInfo dir = new DirectoryInfo(path);
+                FileSystemWatcher watcher = new FileSystemWatcher(path);
+                dir.GetDirectories();
+                treeView1.Nodes.Clear();
+                foreach (var item in dir.GetDirectories())
+                {
+                    TreeNode tn = new TreeNode { Text = item.Name, Name = item.FullName };
+                    treeView1.Nodes.Add(tn);
+                }
+                listView1.Items.Clear();
+                foreach (var item in dir.GetFiles())
+                {
+                    listView1.Items.Add(item.Name);
+                }
+                fileSystemWatcher1.Path = dir.ToString();
+                textBox1.Text = dir.ToString();
+            }
+            else button2.Enabled = true;
+        }
+
+        private void fileSystemWatcher1_Deleted(object sender, FileSystemEventArgs e)
+        {
+            notifyIcon1.Icon = SystemIcons.WinLogo;
+            notifyIcon1.BalloonTipText = e.FullPath;
+            notifyIcon1.BalloonTipTitle = e.ChangeType.ToString();
+            notifyIcon1.ShowBalloonTip(3);
+
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+            FileSystemWatcher watcher = new FileSystemWatcher(textBox1.Text);
+            dir.GetDirectories();
+            treeView1.Nodes.Clear();
+            foreach (var item in dir.GetDirectories())
+            {
+                TreeNode tn = new TreeNode { Text = item.Name, Name = item.FullName };
+                treeView1.Nodes.Add(tn);
+            }
+            listView1.Items.Clear();
+            foreach (var item in dir.GetFiles())
+            {
+                listView1.Items.Add(item.Name);
+            }
+        }
+
+        private void fileSystemWatcher1_Renamed(object sender, RenamedEventArgs e)
+        {
+            notifyIcon1.Icon = SystemIcons.WinLogo;
+            notifyIcon1.BalloonTipText = e.FullPath;
+            notifyIcon1.BalloonTipTitle = e.ChangeType.ToString();
+            notifyIcon1.ShowBalloonTip(3);
+
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+            FileSystemWatcher watcher = new FileSystemWatcher(textBox1.Text);
+            dir.GetDirectories();
+            treeView1.Nodes.Clear();
+            foreach (var item in dir.GetDirectories())
+            {
+                TreeNode tn = new TreeNode { Text = item.Name, Name = item.FullName };
+                treeView1.Nodes.Add(tn);
+            }
+            listView1.Items.Clear();
+            foreach (var item in dir.GetFiles())
+            {
+                listView1.Items.Add(item.Name);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            treeView1.SelectedNode = null;
+            button2.Enabled = false;
+
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode != null)
+                Directory.Delete(treeView1.SelectedNode.Name, true);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StreamWriter save = new StreamWriter("save.txt");
+            save.WriteLine(textBox1.Text);
+            save.Close();
+        }
+    }
+}
+
